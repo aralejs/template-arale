ac05ea415cf09de36db6df04332118af6704e3ea
kangpangpang <kangpangpang@gmail.com>
2012-11-14 13:20:04 +0800

    init

---
+++ dist/template/tests/{{name}}-spec.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+    var {{name}} = require('../src/{{name}}');
+
+    describe('{{name}}', function() {
+
+        test('normal usage', function() {
+
+        });
+    });
+
+});
+
---
+++ src/src/{{name}}.js
@@ -0,0 +1,25 @@
+define(function(require, exports, module) {
+
+    var Widget = require('widget');
+
+    var {{name}} = module.exports = Widget.extend({
+
+        attrs: {
+            'color': 'red'
+        },
+
+        events: {
+            'click h3': 'heading',
+            'mouseover p': 'paragraph'
+        },
+
+        heading: function() {
+            this.$('h3').html('标题被点击了。');
+        },
+
+        paragraph: function() {
+            this.$('p').css('background-color', this.get('color'));
+        }
+    });
+});
+
