22810f62b1c07a4a58639cf985a2f01cc07c5d4b
popomore <sakura9515@gmail.com>
2013-04-20 01:38:50 +0800

    clean

--- dist/template/examples/index.md
+++
@@ -1,9 +0,0 @@
-# 演示文档
-
----
-
-````javascript
-seajs.use('{{name}}', function({{name}}){
-
-});
-````
--- src/src/{{name}}.js
+++
@@ -1,7 +0,0 @@
-define(function(require, exports, module) {
-
-    var {{name}};
-
-    module.exports = {{name}};
-});
-
